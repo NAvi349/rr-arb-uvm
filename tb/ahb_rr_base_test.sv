9408dfa289babd391877430bf7ed52d434f279ae
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-07-27 22:18:51 +0530

    Create ahb_rr_base_test.sv

---
+++ tb/ahb_rr_base_test.sv
@@ -0,0 +1,21 @@
+class ahb_rr_base_test extends uvm_test;
+  `uvm_component_utils(ahb_rr_base_test)
+  
+  ahb_rr_env ahb_rr_env0;
+  
+  function new(string name="ahb_rr_base_test", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+  function void build_phase (uvm_phase phase);
+    super.build_phase(phase);
+    ahb_rr_env0 = ahb_rr_env::type_id::create("ahb_rr_env", this);
+  endfunction
+  
+
+  function void end_of_elaboration ();
+    uvm_top.print_topology();  
+  endfunction
+
+
+endclass
