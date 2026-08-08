f76394e8dc5b95de378f19471e015d59a22c1142
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-08-08 17:03:08 +0530

    Updated with proper flow

--- tb/ahb_rr_if.sv
+++ tb/ahb_rr_if.sv
@@ -1,6 +1,6 @@
 // Have a seperate interface for each master
 
-interface ahb_rr_if0(input clock, input rst_n);
+interface ahb_rr_if0(input clock);
 
   logic hbusreq;
   logic hlock;
@@ -24,7 +24,7 @@ interface ahb_rr_if0(input clock, input rst_n);
   
 endinterface
 
-interface ahb_rr_if1(input clock, input rst_n);
+interface ahb_rr_if1(input clock);
 
   logic   hbusreq;
   logic   hlock;
@@ -49,7 +49,7 @@ interface ahb_rr_if1(input clock, input rst_n);
   
 endinterface
 
-interface ahb_rr_if2(input clock, input rst_n);
+interface ahb_rr_if2(input clock);
 
   logic   hbusreq;
   logic   hlock;
@@ -73,7 +73,7 @@ interface ahb_rr_if2(input clock, input rst_n);
   
 endinterface
 
-interface ahb_rr_if3(input clock, input rst_n);
+interface ahb_rr_if3(input clock);
 
   logic  hbusreq;
   logic  hlock;
@@ -96,8 +96,10 @@ interface ahb_rr_if3(input clock, input rst_n);
     
 endinterface
 
-interface ahb_rr_if(input clock, input rst_n);
-
+interface ahb_rr_if(input clock);
+ 
+  logic rst_n;
+  
   logic hbusreq;
   logic hlock;
   logic [31:0] haddr;
@@ -116,6 +118,42 @@ interface ahb_rr_if(input clock, input rst_n);
   logic [31:0] hrdata;
   logic        hready;
   logic [ 1:0] hresp;
+    
+  
+endinterface
+
+interface ahb_rr_rst_if(input clock);
+  
+  logic rst_n;
+  
+  logic [ 3:0] hbusreq;
+  logic [ 3:0] hlock;
+  
+  logic [31:0] haddr0;
+  logic [31:0] haddr1;
+  logic [31:0] haddr2;
+  logic [31:0] haddr3;
+  
+  
+  logic [ 1:0] htrans0;
+  logic [ 1:0] htrans1;
+  logic [ 1:0] htrans2;
+  logic [ 1:0] htrans3;
+  
+  logic        hwrite0;
+  logic        hwrite1;
+  logic        hwrite2;
+  logic        hwrite3;
+  
+  logic [ 2:0] hsize0;
+  logic [ 2:0] hsize1;
+  logic [ 2:0] hsize2;
+  logic [ 2:0] hsize3;
+  
+  logic [31:0] hwdata0;
+  logic [31:0] hwdata1;
+  logic [31:0] hwdata2;
+  logic [31:0] hwdata3;
     
   
 endinterface
