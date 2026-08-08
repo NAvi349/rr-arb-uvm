af2c4e5fe2ad5f74e647363bb1f2eaa8e6fc0043
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-08-08 17:00:52 +0530

    Updated with proper flow

--- tb/ahb_rr_master_monitor.sv
+++ tb/ahb_rr_master_monitor.sv
@@ -13,7 +13,7 @@ Implementation plan:
 
 class ahb_rr_master_monitor extends uvm_monitor;
   `uvm_component_utils(ahb_rr_master_monitor)
-  
+  int id = 0;  
   virtual ahb_rr_if ahb_xif;
   
   uvm_analysis_port #(ahb_rr_item) dut_tx_out;
@@ -28,6 +28,7 @@ class ahb_rr_master_monitor extends uvm_monitor;
     super.build_phase(phase);
     dut_tx_out = new("dut_m_tx", this);
     uvm_config_db#(virtual ahb_rr_if)::get(this, "", "ahb_rr_if", ahb_xif);
+    uvm_config_db#(int)::get(this, "", "agent_id", id);
   endfunction
   
   
@@ -39,9 +40,11 @@ class ahb_rr_master_monitor extends uvm_monitor;
         forever begin
           //create seq item
           ahb_tx_in = ahb_rr_item::type_id::create("ahb_tx_in", this);
+           @(posedge ahb_xif.clock);
+          //#1ns;
           get_dut_signals();
           get_dut_outputs();
-          `uvm_info("AHB_MON", $sformatf("%s", ahb_tx_in.convert2string_with_index(0)), UVM_HIGH)
+          `uvm_info("AHB_MON", $sformatf("%s", ahb_tx_in.convert2string_with_index(id)), UVM_LOW)
           dut_tx_out.write(ahb_tx_in);
         end
       end
@@ -51,22 +54,22 @@ class ahb_rr_master_monitor extends uvm_monitor;
   endtask
   
   task get_dut_signals();
-    @(posedge ahb_xif.clock);
-    ahb_tx_in.hbusreq <= ahb_xif.hbusreq;
-    ahb_tx_in.hlock   <= ahb_xif.hlock;
-    ahb_tx_in.haddr   <= ahb_xif.haddr;
-    ahb_tx_in.htrans  <= ahb_xif.htrans;
-    ahb_tx_in.hwrite  <= ahb_xif.hwrite;
-    ahb_tx_in.hsize   <= ahb_xif.hsize;
-    ahb_tx_in.hwdata  <= ahb_xif.hwdata;
+
+    ahb_tx_in.hbusreq = ahb_xif.hbusreq;
+    ahb_tx_in.hlock   = ahb_xif.hlock;
+    ahb_tx_in.haddr   = ahb_xif.haddr;
+    ahb_tx_in.htrans  = ahb_xif.htrans;
+    ahb_tx_in.hwrite  = ahb_xif.hwrite;
+    ahb_tx_in.hsize   = ahb_xif.hsize;
+    ahb_tx_in.hwdata  = ahb_xif.hwdata;
   endtask
   
   task get_dut_outputs();
-    @(posedge ahb_xif.clock);
-    ahb_tx_in.hgrant  <= ahb_xif.hgrant;
-    ahb_tx_in.hrdata  <= ahb_xif.hrdata;
-    ahb_tx_in.hready  <= ahb_xif.hready;
-    ahb_tx_in.hresp   <= ahb_xif.hresp;
+    //@(posedge ahb_xif.clock);
+    ahb_tx_in.hgrant  = ahb_xif.hgrant;
+    ahb_tx_in.hrdata  = ahb_xif.hrdata;
+    ahb_tx_in.hready  = ahb_xif.hready;
+    ahb_tx_in.hresp   = ahb_xif.hresp;
  endtask 
   
 endclass
