af5f9096860565eedf6a17e0c0b6e6ed35bda2d6
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-08-08 17:07:53 +0530

    Updated with proper flow

--- tb/ahb_rr_seq_lib.sv
+++ tb/ahb_rr_seq_lib.sv
@@ -23,8 +23,11 @@ class ahb_rr_master_sequence extends uvm_sequence #(ahb_rr_item);
     // send addr + data in single sequence item
     `uvm_info(get_full_name(), "Masterside item", UVM_LOW)
     start_item(ahb_m_tx);  // request grant
-               
+    
     ahb_m_tx.randomize();
+    $display("%h addr " , ahb_m_tx.haddr);
+    
+    //ahb_m_tx.convert2string_with_index();
     
     finish_item(ahb_m_tx);
     
@@ -32,3 +35,27 @@ class ahb_rr_master_sequence extends uvm_sequence #(ahb_rr_item);
   
   
 endclass: ahb_rr_master_sequence
+
+// create reset sequence
+
+class ahb_rr_reset_sequence extends ahb_rr_master_sequence #(ahb_rr_item);
+  
+ `uvm_object_utils(ahb_rr_reset_sequence)
+  
+  function new(string name = "ahb_rr_reset_sequence");
+    super.new(name);
+    
+  endfunction: new
+  
+  task body();
+    ahb_m_tx = ahb_rr_item::type_id::create("ahb_m_tx");
+    
+    start_item(ahb_m_tx);
+    
+    ahb_m_tx.randomize with { haddr == 0; hwdata == 0; hlock == 0; hbusreq == 0; };
+    
+    finish_item(ahb_m_tx);
+   
+  endtask
+  
+endclass: ahb_rr_reset_sequence
