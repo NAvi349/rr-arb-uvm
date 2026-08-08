c73fd7848d6a760404895879f8e0174ff66b0631
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-08-08 17:08:37 +0530

    Updated with proper flow

--- tb/ahb_rr_write_read_test.sv
+++ tb/ahb_rr_write_read_test.sv
@@ -1,8 +1,11 @@
 class ahb_rr_write_read_test extends ahb_rr_base_test;
   `uvm_component_utils(ahb_rr_write_read_test )
 
-  ahb_rr_master_sequence ahb_rr_m_seq;
-  
+  //ahb_rr_reset_sequence ahb_rr_r_seq;
+  ahb_rr_master_sequence ahb_rr_m_seq_0;
+  ahb_rr_master_sequence ahb_rr_m_seq_1;
+  ahb_rr_master_sequence ahb_rr_m_seq_2;  
+  ahb_rr_master_sequence ahb_rr_m_seq_3;
   
   function new(string name="ahb_rr_write_read_test", uvm_component parent);
     super.new(name, parent);
@@ -10,7 +13,12 @@ class ahb_rr_write_read_test extends ahb_rr_base_test;
   
   function void build_phase (uvm_phase phase);
     super.build_phase(phase);
-    ahb_rr_m_seq = ahb_rr_master_sequence::type_id::create("ahb_rr_m_seq");
+    //ahb_rr_r_seq   = ahb_rr_reset_sequence::type_id::create("ahb_rr_r_seq");
+    ahb_rr_m_seq_0 = ahb_rr_master_sequence::type_id::create("ahb_rr_m_seq_0");
+    ahb_rr_m_seq_1 = ahb_rr_master_sequence::type_id::create("ahb_rr_m_seq_1");
+    ahb_rr_m_seq_2 = ahb_rr_master_sequence::type_id::create("ahb_rr_m_seq_2");
+    ahb_rr_m_seq_3 = ahb_rr_master_sequence::type_id::create("ahb_rr_m_seq_3");
+    
   endfunction
   
   task run_phase (uvm_phase phase);
@@ -19,12 +27,22 @@ class ahb_rr_write_read_test extends ahb_rr_base_test;
     phase.raise_objection(this);
     
     `uvm_info(get_full_name, "Starting seq", UVM_LOW)
-    //fork
-    ahb_rr_m_seq.start(ahb_rr_env0.ahb_m_agt[0].ahb_rr_m_sqr);
-    ahb_rr_m_seq.start(ahb_rr_env0.ahb_m_agt[1].ahb_rr_m_sqr);
-    ahb_rr_m_seq.start(ahb_rr_env0.ahb_m_agt[2].ahb_rr_m_sqr);
-    ahb_rr_m_seq.start(ahb_rr_env0.ahb_m_agt[3].ahb_rr_m_sqr);
-    //join
+    
+    //ahb_rr_r_seq.start(ahb_rr_env0.ahb_rr_agt.ahb_rr_rst_sqr);
+    
+    fork
+      ahb_rr_m_seq_0.start(ahb_rr_env0.ahb_m_agt[0].ahb_rr_m_sqr);
+      ahb_rr_m_seq_1.start(ahb_rr_env0.ahb_m_agt[1].ahb_rr_m_sqr);
+      ahb_rr_m_seq_2.start(ahb_rr_env0.ahb_m_agt[2].ahb_rr_m_sqr);
+      ahb_rr_m_seq_3.start(ahb_rr_env0.ahb_m_agt[3].ahb_rr_m_sqr);
+    join
+    
+    fork
+      ahb_rr_m_seq_0.start(ahb_rr_env0.ahb_m_agt[0].ahb_rr_m_sqr);
+      ahb_rr_m_seq_1.start(ahb_rr_env0.ahb_m_agt[1].ahb_rr_m_sqr);
+      ahb_rr_m_seq_2.start(ahb_rr_env0.ahb_m_agt[2].ahb_rr_m_sqr);
+      ahb_rr_m_seq_3.start(ahb_rr_env0.ahb_m_agt[3].ahb_rr_m_sqr);
+    join
     
     
     //#400ns;
