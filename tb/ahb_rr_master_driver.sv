2743ebc8ba1ad9713667c8a69d0c833c91160420
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-08-08 16:54:41 +0530

    Updated with proper flow

--- tb/ahb_rr_master_driver.sv
+++ tb/ahb_rr_master_driver.sv
@@ -5,6 +5,7 @@ Description: This is the driver of input. The driver respects the AHB protocol.
 class ahb_rr_master_driver extends uvm_driver #(ahb_rr_item);
 
   `uvm_component_utils(ahb_rr_master_driver)
+    int id = 0;
   virtual ahb_rr_if xif;
    
   //uvm_sequence_item tx;
@@ -18,55 +19,111 @@ class ahb_rr_master_driver extends uvm_driver #(ahb_rr_item);
   function void build_phase (uvm_phase phase);
     super.build_phase(phase);
     uvm_config_db#(virtual ahb_rr_if)::get(this, "", "ahb_rr_if", xif);
+        uvm_config_db#(int)::get(this, "", "agent_id", id);
   endfunction
-  
   
   task run_phase (uvm_phase phase);
     
-    `uvm_info("INPUT_DRV", "Waiting for reset release", UVM_LOW)
+   `uvm_info("INPUT_DRV", "Starting driver", UVM_LOW)
     
     reset_dut();
-
+    
     forever begin
-      `uvm_info("INPUT_DRV", "Getting next item from sequence in input driver", UVM_LOW)
+     `uvm_info("INPUT_DRV", "Getting next item from sequence in input driver", UVM_LOW)
       seq_item_port.get_next_item(i_tx);
-
-      drive_address();
-      drive_data();
       drive_request();
-      while (xif.hgrant === 0) begin
+      
+      fork
+        begin
+          while (xif.hgrant === 0) begin
+           `uvm_info("INPUT_DRV", "Waiting for grant", UVM_LOW)
+            @(posedge xif.clock);
+          end
+        end
+        
+        begin
+          repeat (100) @(posedge xif.clock);
+          `uvm_info("INPUT_DRV", "Grant not got after 100 clock cycles", UVM_LOW)
+        end
+        
+      join_any
+      
+      disable fork;
+      if (xif.hgrant === 1) begin
+        drive_address();
+        drive_data();
         @(posedge xif.clock);
+            xif.hbusreq <= 0;
+        
+        fork
+          begin
+            while (xif.hready != 1) begin
+              `uvm_info("INPUT_DRV", "Hready not asserted yet", UVM_LOW)
+              @(posedge xif.clock);  
+            end
+          end
+          
+          begin
+            repeat (4) @(xif.clock);
+            `uvm_info("INPUT_DRV", "Hready not asserted till timeout", UVM_LOW)
+          end
+        join_any
+
       end      
+
+        
       seq_item_port.item_done();
     end
     
   endtask
   
+        
   virtual task reset_dut();
-    @(posedge xif.rst_n);
     
-    `uvm_info("INPUT_DRV", "Reset released", UVM_LOW)
+    `uvm_info("MST_DRV", "Reset driven, initializing DUT inputs", UVM_LOW)
+    xif.hwrite  <= 'h0;
+    xif.hbusreq <= 'h0;
+    xif.haddr   <= 'h0;
+    xif.hsize   <= 'h0;
+    xif.htrans  <= 'h0;
+    xif.hwdata  <= 'h0;
+    
+    fork 
+      begin
+        wait (xif.rst_n);
+       `uvm_info("MST_DRV", "Reset released", UVM_LOW)
+      end
+      
+      begin
+        repeat (20) @(posedge xif.clock);
+        `uvm_fatal("MST_DRV", "Initial Reset not released, ending simulation")
+      end
+    join_any
+    
+
   endtask
   
   virtual task drive_request();
-    xif.hbusreq <= 'h1;
-    @(xif.clock);
-    `uvm_info("MST_DRV", "Drove bus request", UVM_LOW)
+    @(posedge xif.clock);
+    xif.hbusreq <= 'b1;
+    xif.hlock   <= 'b0;
+   `uvm_info("MST_DRV", "Drove bus request", UVM_LOW)
   endtask
   
   virtual task drive_address();
-    xif.haddr  <= 'h0000_0004;
-    xif.htrans <= 'h10;
-    xif.hwrite <= 'h1;
-    xif.hsize  <= 'b001;
-    @(xif.clock);
-    `uvm_info("MST_DRV", "Drove bus driver", UVM_LOW)
+    @(posedge xif.clock);
+    xif.haddr  <= i_tx.haddr;
+    xif.htrans <= 'b10;
+    xif.hwrite <= 1;
+    xif.hsize  <= 0;
+   `uvm_info("MST_DRV", "Drove bus address", UVM_LOW)
   endtask
   
   virtual task drive_data();
-    xif.hwdata <= 'h0010_2234;    
-    @(xif.clock);
-    `uvm_info("MST_DRV", "Drove drive data", UVM_LOW)
+    @(posedge xif.clock);
+    xif.hwdata <= i_tx.hwdata;
+
+   `uvm_info("MST_DRV", "Drove bus data", UVM_LOW)
   endtask
  
   endclass: ahb_rr_master_driver
